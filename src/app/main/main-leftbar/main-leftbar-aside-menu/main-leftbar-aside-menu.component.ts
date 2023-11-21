e5053f3ec29256e0902af2a2fbcb8f0e5c966506
hellothereimandrew <90530914+hellothereimandrew@users.noreply.github.com>
2023-11-21 22:41:42 +0300

    ez-speak

--- src/app/main/main-leftbar/main-leftbar-aside-menu/main-leftbar-aside-menu.component.ts
+++ src/app/main/main-leftbar/main-leftbar-aside-menu/main-leftbar-aside-menu.component.ts
@@ -8,5 +8,27 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 export class MainLeftbarAsideMenuComponent implements OnInit {
   @Output() public hideOptions: EventEmitter<void> = new EventEmitter<void>();
 
+  public activeTheme: boolean = false;
+  public fontFamily: string = 'Nunito-Light';
+  public themeName: string = 'dark';
+
   ngOnInit(): void {}
+
+  public setAppTheme(themeName: string = 'dark'): string {
+    localStorage.setItem('theme', themeName);
+
+    if (localStorage.getItem('theme') === 'dark') {
+      themeName = 'dark';
+    }
+
+    if (localStorage.getItem('theme') === 'light') {
+      themeName = 'light';
+    }
+
+    if (localStorage.getItem('theme') === 'combined') {
+      themeName = 'combined';
+    }
+
+    return themeName;
+  }
 }
