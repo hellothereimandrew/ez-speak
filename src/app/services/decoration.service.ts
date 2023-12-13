09e8c1dbad088755b7090fa51a71b8a8930b6372
hellothereimandrew <90530914+hellothereimandrew@users.noreply.github.com>
2023-12-13 16:12:15 +0300

    Рефакторим decoration.service

--- src/app/services/decoration.service.ts
+++ src/app/services/decoration.service.ts
@@ -4,31 +4,11 @@ import {Injectable} from '@angular/core';
   providedIn: 'root',
 })
 export class DecorationService {
-  public fontFamily: string = '';
-  public fontColor: string = '';
-  public currentTheme: string = '';
+  public setAppTheme(themeName: string): void {
+    localStorage.setItem('theme', themeName);
+  }
 
-  public setAppTheme(currentTheme: string): string {
-    localStorage.setItem('theme', currentTheme);
-
-    if (localStorage.getItem('theme') === 'dark-theme') {
-      this.currentTheme = currentTheme;
-      this.fontFamily = 'Nunito-Light';
-      this.fontColor = '#f0f0f0';
-    }
-
-    if (localStorage.getItem('theme') === 'light-theme') {
-      this.currentTheme = currentTheme;
-      this.fontFamily = 'Nunito-Light';
-      this.fontColor = '#f0f0f0';
-    }
-
-    if (localStorage.getItem('theme') === 'combined-theme') {
-      this.currentTheme = currentTheme;
-      this.fontFamily = 'Nunito-Light';
-      this.fontColor = '#f0f0f0';
-    }
-
-    return currentTheme;
+  public getAppTheme(): string {
+    return localStorage.getItem('theme');
   }
 }
