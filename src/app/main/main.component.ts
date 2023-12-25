aee0fa5bcca84569aa6add28e7c1e8fd9c6e36fd
hellothereimandrew <90530914+hellothereimandrew@users.noreply.github.com>
2023-12-25 19:44:49 +0300

    Прикручиваем темы

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,25 +1,32 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {Chat} from 'src/app/shared/chat-db';
 import {DecorationService} from '../services/decoration.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements OnInit {
-  constructor(private decoreationServise: DecorationService) {}
-
+export class MainComponent implements OnInit, OnDestroy {
   @Input() public currentChat: Chat = {
     id: 0,
     ico: '',
     name: '',
   };
 
+  constructor(private decoreationServise: DecorationService) {}
+
   public hideChatSection: boolean = true;
   public hideRightbar: boolean = true;
+  public selectedTheme: string = '';
+  public themeSubscription: Subscription = new Subscription();
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.themeSubscription = this.decoreationServise.selectedTheme$.subscribe((theme) => {
+      this.selectedTheme = theme;
+    });
+  }
 
   public showChatSection(): void {
     this.hideChatSection = !this.hideChatSection;
@@ -31,5 +38,9 @@ export class MainComponent implements OnInit {
 
   public showRightbar(): void {
     this.hideRightbar = !this.hideRightbar;
+  }
+
+  ngOnDestroy(): void {
+    this.themeSubscription.unsubscribe();
   }
 }
